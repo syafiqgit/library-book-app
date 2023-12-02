6144bfd404f788cb0141ab04f54376671f57a718
Syafiq <syafiqsteam744@gmail.com>
2023-12-02 19:48:55 +0700

    feat: add protected routes

--- src/routes/protected-routes.tsx
+++ src/routes/protected-routes.tsx
@@ -1,11 +1,25 @@
+import { useToken } from "@/utils/contexts/token";
 import { Outlet, Navigate, useLocation } from "react-router-dom";
 
 export default function ProtectedRoutes() {
   const { pathname } = useLocation();
+  const { token, user } = useToken();
 
   const authProtected = ["/login", "/register"];
   const tokenProtected = ["/profile", "/profile/edit", "/dashboard"];
   const roleProtected = ["/dashboard"];
+
+  if (authProtected.includes(pathname)) {
+    if (token) return <Navigate to="/" />;
+  }
+
+  if (tokenProtected.includes(pathname)) {
+    if (!token) return <Navigate to="/login" />;
+
+    if (roleProtected.includes(pathname)) {
+      if (user.role === "user") return <Navigate to="/" />;
+    }
+  }
 
   return <Outlet />;
 }
